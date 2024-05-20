a117c96a61927e2710edf73121a47b76704d2933
MR-FARHAT <142103811+MR-FARHAT@users.noreply.github.com>
2024-05-20 21:14:25 +0300

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,180 +1,103 @@
-import { useFormik } from "formik";
-import React, { useState } from "react";
-import * as Yup from "yup";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+const validationSchema = Yup.object({
+  name: Yup.string().required("required field").min(3, "the minimum required field is three characters").max(20, "the maxmum required field is 20 characters"),
 
+  email: Yup.string().required("required field").matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i, "pleas writ the correct email address"),
+  phone: Yup.string().required("required field").matches(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/i, "pleas writ the correct phone number"),
+  password: Yup.string().required("required field").matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/i, "Minimum eight characters, at least one letter, one number and one special character:"),
+  rePassword: Yup.string().required("required field").oneOf([Yup.ref("password")], "should be matches with password")
+
+
+})
+const initialValues = {
+  name: '',
+  email: '',
+  phone: '',
+  password: '',
+  rePassword: ''
+}
+
+// const validate = (values) => {
+//     let errors = {}
+//     if (!values.name) {
+//         errors.name = "required field"
+//     } else if (values.name === "") {
+//         errors.name = "required field"
+//     }
+//     else if (values.name.length < 3) {
+//         errors.name = "the minimum required field is three characters"
+//     }
+//     console.log(errors)
+//     return errors
+
+// }
 export default function Register() {
-  const [errorMsg, setErrorMsg] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const navigateToLogin = useNavigate();
-
-  const validationSchema = Yup.object({
-    name: Yup.string()
-      .required("Name is required")
-      .min(5, "Name must be more than 3 charcters")
-      .max(20, "Name must be less than 20 charcters"),
-    email: Yup.string()
-      .required("Email is required")
-      .matches(
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Enter valid Email"
-      ),
-    password: Yup.string()
-      .required("Password is required")
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-        "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-      ),
-    rePassword: Yup.string()
-      .required("Repassword is required")
-      .oneOf([Yup.ref("password")]),
-    phone: Yup.string().required("Phone number is required"),
-  });
-
-  
-
-  const {
-    values,
-    handleSubmit,
-    handleChange,
-    errors,
-    touched,
-    handleBlur,
-    isValid,
-  } = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      password: "",
-      rePassword: "",
-      phone: "",
-    },
+  const [err, seterr] = useState('')
+  const [isload, setload] = useState(false)
+  const navigate = useNavigate('')
+  const { handleChange, values, handleSubmit, errors, handleBlur, touched, isValid } = useFormik({
+    initialValues,
     onSubmit: async () => {
-      setErrorMsg("");
-
+      seterr('')
+      setload(true)
       try {
-        setIsLoading(true);
-        let { data } = await axios.post(
-          "https://ecommerce.routemisr.com/api/v1/auth/signup",
-          values
-        );
+        let { data } = await axios.post('https://ecommerce.routemisr.com/api/v1/auth/signup', values)
+        console.log(data)
+        // eslint-disable-next-line eqeqeq
         if (data.message == "success") {
-          navigateToLogin("/login");
+          navigate('/login')
         }
-        console.log(data);
+
       } catch (error) {
-        setErrorMsg(  );
+        seterr(error.response.data.message)
+        // let err = error.response.data.message
+        // eslint-disable-next-line no-unused-expressions
+        // err ? window.alert(err) : ""
       }
-      setIsLoading(false);
+      setload(false)
     },
-    validationSchema,
-  });
+    validationSchema
+
+
+  })
+
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Name"
-            value={values.name}
-            onChange={handleChange}
-            id="name"
-            onBlur={handleBlur}
-          />
-          {errors.name && touched.name && (
-            <p className="alert alert-danger">{errors.name}</p>
-          )}
-        </div>
+    <>
 
-        <div className="mb-3">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            value={values.email}
-            onChange={handleChange}
-            id="email"
-            onBlur={handleBlur}
-          />
-          {errors.email && touched.email && (
-            <p className="alert alert-danger">{errors.email}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            value={values.password}
-            onChange={handleChange}
-            id="password"
-            onBlur={handleBlur}
-          />
-          {errors.password && touched.password && (
-            <p className="alert alert-danger">{errors.password}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="rePassword">Repassword</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Repassword"
-            value={values.rePassword}
-            onChange={handleChange}
-            id="rePassword"
-            onBlur={handleBlur}
-          />
-          {errors.rePassword && touched.rePassword && (
-            <p className="alert alert-danger">{errors.rePassword}</p>
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Phone"
-            value={values.phone}
-            onChange={handleChange}
-            id="phone"
-            onBlur={handleBlur}
-          />
-          {errors.phone && touched.phone && (
-            <p className="alert alert-danger">{errors.phone}</p>
-          )}
-        </div>
-        {errorMsg && <div className="alert alert-danger">{errorMsg}</div>}
 
-        <div className="d-grid">
-          {isLoading ? (
-            <button
-              disabled
-              type="button"
-              className="btn px-4 text-primary signup-btn"
-            >
-              <i class="fa-solid fa-sync fa-spin"></i>
-            </button>
-          ) : (
-            <button
-              disabled={!isValid || isLoading}
-              type="submit"
-              className="btn btn-primary signup-btn"
-            >
-              Sign Up
-            </button>
-          )}
-        </div>
-        <p className="forgot-password text-right mt-3 ">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
-    </div>
-  );
+      <div className="col-md-4 m-auto shadow-lg p-5">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name" className="form-label mt-2">your name</label>
+          <input onBlur={handleBlur} type="text" className="form-control " id="name" name="name" onChange={handleChange} value={values.name} />
+          {errors.name && touched.name && <p className="alert alert-warning my-2">{errors.name}</p>}
+
+          <label htmlFor="email" className="form-label mt-2">Email</label>
+          <input onBlur={handleBlur} type="email" className="form-control " id="email" name="email" onChange={handleChange} value={values.email} />
+          {errors.email && touched.email && <p className="alert alert-warning my-2">{errors.email}</p>}
+          <label htmlFor="phone" className="form-label mt-2">Phone number</label>
+          <input onBlur={handleBlur} type="text" className="form-control " id="phone" name="phone" onChange={handleChange} value={values.phone} />
+          {errors.phone && touched.phone && <p className="alert alert-warning my-2">{errors.phone}</p>}
+          <label htmlFor="pass-1" className="form-label mt-2">Password</label>
+          <input onBlur={handleBlur} type="password" className="form-control " id="pass-1" name="password" onChange={handleChange} value={values.password} />
+          {errors.password && touched.password && <p className="alert alert-warning my-2">{errors.password}</p>}
+          <label htmlFor="pass-2" className="form-label mt-2">repassword</label>
+          <input onBlur={handleBlur} type="password" className="form-control " id="pass-2" name="rePassword" onChange={handleChange} value={values.rePassword} />
+          {errors.rePassword && touched.rePassword && <p className="alert alert-warning my-2">{errors.rePassword}</p>}
+          {isload ? <button type="submit" disabled="" className="btn bg-main ms-auto my-2 px-4 d-block"><i className="px-2 fa-solid fa-spinner fa-spin"></i></button>
+            :
+            <button type="submit" disabled={!isValid || isload} className="btn btn-outline-success ms-auto my-2 d-block">SUBMIT</button>
+          }
+
+
+          {err && <div className="alert alert-danger">{err}</div>}
+        </form>
+      </div>
+
+
+    </>
+  )
 }
